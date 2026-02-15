d84a52a8cdcdc7542506706cee5204c76eefda02
meenakshi-sm914 <me240003054@iiti.ac.in>
2026-02-16 01:27:47 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,21 @@
+// Fade-in animation on scroll
+const faders = document.querySelectorAll('.fade-in');
+
+const appearOptions = {
+    threshold: 0.2
+};
+
+const appearOnScroll = new IntersectionObserver(function(entries, observer) {
+    entries.forEach(entry => {
+        if (!entry.isIntersecting) {
+            return;
+        } else {
+            entry.target.classList.add('show');
+            observer.unobserve(entry.target);
+        }
+    });
+}, appearOptions);
+
+faders.forEach(fader => {
+    appearOnScroll.observe(fader);
+});
